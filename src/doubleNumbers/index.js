38fbb40ff6eaad060080bed92894344e93bc68c2
SinfulS0ul <pasichnikmaks@gmail.com>
2018-11-01 16:26:07 +0200

    Homework done

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,8 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
   // your logic here...
+  if(String(num).substring(0,String(num).length/2|0) === String(num).substring(String(num).length/2|0))
+    return num;
+  else
+  	return num*2;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,12 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
   // your logic here...
+  function toNum(num){
+  	return num.split('').reduce((sum, curr)=>{
+    	return Number(sum) + Number(curr);
+    })
+  }
+  return arr.sort((a, b)=>{
+  	return toNum(a) - toNum(b);
+  })
 };
